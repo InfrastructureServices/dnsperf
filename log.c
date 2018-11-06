7d437f598f429dc64527767599a018a2f69b1f2a
Petr Menšík <pemensik@redhat.com>
2018-11-06 16:48:34 +0100

    Tarball contents 2.1.0.0
    Fetched from http://wwwns.akamai.com/dnsperf-src-2.1.0.0-1.tar.gz

--- log.c
+++ log.c
@@ -1,17 +1,18 @@
 /*
- * Copyright (C) 2011-2015 Nominum, Inc.
+ * Copyright (C) 2011 - 2015 Nominum, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice
+ * appear in all copies.
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND NOMINUM DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL NOMINUM BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <pthread.h>
@@ -27,36 +28,36 @@ pthread_mutex_t log_lock = PTHREAD_MUTEX_INITIALIZER;
 static void
 vlog(FILE *stream, const char *prefix, const char *fmt, va_list args)
 {
-    LOCK(&log_lock);
-    fflush(stdout);
-    if (prefix != NULL)
-        fprintf(stream, "%s: ", prefix);
-    vfprintf(stream, fmt, args);
-    fprintf(stream, "\n");
-    UNLOCK(&log_lock);
+	LOCK(&log_lock);
+	fflush(stdout);
+	if (prefix != NULL)
+		fprintf(stream, "%s: ", prefix);
+	vfprintf(stream, fmt, args);
+	fprintf(stream, "\n");
+	UNLOCK(&log_lock);
 }
 
 void
 perf_log_printf(const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-    vlog(stdout, NULL, fmt, args);
+	va_list args;
+	va_start(args, fmt);
+	vlog(stdout, NULL, fmt, args);
 }
 
 void
 perf_log_fatal(const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-    vlog(stderr, "Error", fmt, args);
-    exit(1);
+	va_list args;
+	va_start(args, fmt);
+	vlog(stderr, "Error", fmt, args);
+	exit(1);
 }
 
 void
 perf_log_warning(const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-    vlog(stderr, "Warning", fmt, args);
+	va_list args;
+	va_start(args, fmt);
+	vlog(stderr, "Warning", fmt, args);
 }
--- net.h
+++ net.h
@@ -1,17 +1,35 @@
 /*
- * Copyright (C) 2000-2001,2004-2015 Nominum, Inc.
+ * Copyright (C) 2000, 2001  Nominum, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * THE SOFTWARE IS PROVIDED "AS IS" AND INTERNET SOFTWARE CONSORTIUM
+ * DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ * INTERNET SOFTWARE CONSORTIUM BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
+ * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * Copyright (C) 2004 - 2015 Nominum, Inc.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice
+ * appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND NOMINUM DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL NOMINUM BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #ifndef PERF_NET_H
@@ -22,14 +40,14 @@ perf_net_parsefamily(const char *family);
 
 void
 perf_net_parseserver(int family, const char *name, unsigned int port,
-                     isc_sockaddr_t *addr);
+		     isc_sockaddr_t *addr);
 
 void
 perf_net_parselocal(int family, const char *name, unsigned int port,
-                    isc_sockaddr_t *addr);
+		    isc_sockaddr_t *addr);
 
 int
 perf_net_opensocket(const isc_sockaddr_t *server, const isc_sockaddr_t *local,
-                    unsigned int offset, int bufsize);
+		    unsigned int offset, int bufsize);
 
 #endif
